6ddec74e0e30beacdbd79d8823a754e7e84bcd59
Balazs Zagyvai <415627+zagyi@users.noreply.github.com>
2026-07-25 20:46:06 +0200

    Create install.sh

---
+++ site/install.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env zsh
+
+set -eu
+
+function error() {
+  echo "[$(date "+%Y-%m-%dT%H:%M:%S%z")]: $*" >&2
+}
+
+function log() {
+  printf '[bootstrap] %s\n' "$*" >&2
+}
+
+function install_xcode_clt() {
+  if [ "$(uname -s)" = "Darwin" ]; then
+    log "Detected macOS."
+    
+    # Check if Xcode Command Line Tools are already installed
+    if xcode-select -p >/dev/null 2>&1; then
+      log "Xcode Command Line Tools are already installed."
+    else
+      log "Triggering Xcode Command Line Tools installation..."
+      
+      xcode-select --install
+
+      log "Waiting for Xcode Command Line Tools installation to complete..."
+      until xcode-select -p >/dev/null 2>&1; do
+        sleep 2
+      done
+
+      log "Xcode Command Line Tools installed successfully!"
+    fi
+  fi
+}
+
+# Installs Homebrew package manager on macOS and Linux if it is not already
+# installed.
+function install_homebrew() {
+  local HOMEBREW_PKG_PATH="/tmp/homebrew.pkg"
+
+  if command -v brew &> /dev/null; then
+    log "Homebrew is already installed at $(where brew)."
+  else
+    log "Installing Homebrew..."
+    local OS
+    OS="$(uname -s)"
+
+    case "${OS}" in
+      "Darwin")
+        # Install with `.pkg` installer, introduced in Homebrew 4.1.2. Since
+        # Apple requires by default that all software installed via `installer`
+        # is signed by a developer certificate issued by Apple, I consider this
+        # method more secure than running the convenience script.
+        log "Detected macOS, installing Homebrew with homebrew.pkg from Homebrew/brew..."
+
+        release_json=$(
+          curl \
+            --fail \
+            --location \
+            --show-error \
+            --silent \
+            --header "Accept: application/vnd.github+json" \
+            --header "X-GitHub-Api-Version: 2026-03-10" \
+            https://api.github.com/repos/Homebrew/brew/releases/latest
+        )
+        release_asset_json=$(jq '.assets[] | select(.name | endswith ("pkg"))' <<< $release_json)
+        asset_name=$(jq '.name' --raw-output <<< $release_asset_json)
+        asset_browser_download_url=$(jq '.browser_download_url' --raw-output <<< $release_asset_json)
+        log "Downloading ${asset_name}..."
+        log "-> ${asset_browser_download_url}"
+
+        curl --fail --location --show-error --silent --output "${HOMEBREW_PKG_PATH}" \
+          "${asset_browser_download_url}"
+
+        if (($? != 0)); then
+          error "Failed to download Homebrew package installer from \
+${asset_browser_download_url}."
+          return 1
+        fi
+
+        if [[ -f "${HOMEBREW_PKG_PATH}" ]]; then
+          log "Homebrew package installer downloaded to ${HOMEBREW_PKG_PATH}"
+        else
+          error "Homebrew package installer download failed. File was not \
+found at ${HOMEBREW_PKG_PATH}."
+          return 1
+        fi
+
+        log "Installing Homebrew..."
+        sudo installer -package "${HOMEBREW_PKG_PATH}" -target "/"
+
+        if command pkgutil --pkg-info "sh.brew.homebrew" &> /dev/null; then
+          log "Homebrew installed successfully to $(builtin where brew)."
+          rm -f "${HOMEBREW_PKG_PATH}"
+        else
+          error "Homebrew installation failed. Please confirm that the file at \
+${HOMEBREW_PKG_PATH} is a valid package. You may download the .pkg file \
+manually at https://github.com/homebrew/brew/releases/latest."
+          return 1
+        fi
+        ;;
+
+      "Linux")
+        # Install with Bash shell script (https://github.com/Homebrew/install)
+        log "Detected Linux, installing Homebrew with Bash shell script..."
+        NONINTERACTIVE=1 /bin/bash -c "$(
+          curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+        )"
+        log "Installation script ran successfully."
+        ;;
+
+      *)
+        error "Unexpected OS: ${OS}. Homebrew is only supported on macOS and Linux."
+        return 1
+        ;;
+    esac
+  fi
+}
+
+install_xcode_clt
+install_homebrew
+brew install chezmoi
+
+chezmoi init zagyi --apply
